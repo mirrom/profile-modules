95772617268cda75df4f1c4687cab9a7212c85e8
Mirko Romstadt <mirko.romstadt@kex-ag.com>
2021-02-26 17:41:17 +0100

    Fix QueryDSL repositories

--- profile-service/src/main/java/com/example/profile/repository/sub1profile/Sub1ProfileRepository.java
+++ profile-service/src/main/java/com/example/profile/repository/sub1profile/Sub1ProfileRepository.java
@@ -1,27 +1,29 @@
 package com.example.profile.repository.sub1profile;
 
+import com.example.profile.model.sub1profile.QSub1Profile;
 import com.example.profile.model.sub1profile.Sub1Profile;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringPath;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.SingleValueBinding;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface Sub1ProfileRepository extends MongoRepository<Sub1Profile, ObjectId> {
-
-}
-
-/*
 public interface Sub1ProfileRepository
         extends MongoRepository<Sub1Profile, ObjectId>, QuerydslPredicateExecutor<Sub1Profile>,
         QuerydslBinderCustomizer<QSub1Profile> {
     
-    default void customize(QuerydslBindings bindings, QProfile root) {
+    @Override
+    default void customize(QuerydslBindings bindings, QSub1Profile root) {
         
         bindings.bind(String.class)
                 .first((SingleValueBinding<StringPath, String>) StringExpression::containsIgnoreCase);
     }
     
 }
-*/
